a892e850f916b17ed5539520d1f1516f360c0ed6
Andrew <degon1997@hotmail.com>
2019-01-08 02:20:51 -0500

    added some gameplay
    TODO:
        *Set winning condition
        *Add more rules

--- Solitaire/solitaire.py
+++ Solitaire/solitaire.py
@@ -16,9 +16,6 @@ for suit in suits:
 
 random.shuffle(deck.S) #literally shuffles it
 
-# for x in range(5):
-# 	print(deck.S[x][3])
-
 emptystack = []#position 1
 boardstack = []#position 2
 cardshownstack = Stack()#display must be capped at 3 elements .. position 3
@@ -35,13 +32,17 @@ def display_card(deck, card_in_deck):
 	num = 0
 	suit = 1
 	pos = 3#so i don't have to change everything
+	
 	if (deck.isEmpty()):
 		return "[    ]"
-	elif (deck.S[card_in_deck][pos]):
-		return "[~XX~]"
+	
+	if (deck.S[card_in_deck][pos]):
+		return "[~  ~]"
 	else:
 		if(deck.S[card_in_deck][num] == 1):
 			return("[~A"+deck.S[card_in_deck][suit]+"~]")
+		elif(deck.S[card_in_deck][num] == 10):
+			return("[~X"+deck.S[card_in_deck][suit]+"~]")
 		elif(deck.S[card_in_deck][num] == 11):
 			return("[~J"+deck.S[card_in_deck][suit]+"~]")
 		elif(deck.S[card_in_deck][num] == 12):
@@ -62,8 +63,7 @@ def switch_postion(stack):	#shows card
 	stack.S[stack.top][3] = False
 
 def compare_color(stack, card):
-	# stack.S[stack.top].suit
-	# card.suit
+	suit = 1
 	if (card[suit] == stack.S[stack.top][suit]):
 		return 1 #same suit for emptystack
 	#allow to be placed in boardstack
@@ -74,33 +74,105 @@ def compare_color(stack, card):
 	else:
 		return False #unvalid move
 
+def check_location(stack1):#detination
+	location = 2
+	if(stack1.S[stack1.top][location] == 2 ):
+		return 2 #to board
+	elif(stack1.S[stack1.top][location] == 1):
+		return 1 #to empty
+	else:
+		return False #unvalid move (cannot be moved to deck or shown)
+
 #gameplay will be added		
 for x in range(len(boardstack)):
-	for c in range(x+1):
-		move_cards(deck,boardstack[c])
-		switch_location(boardstack[c], 2)
+	for c in reversed(range(x + 1)):
+		move_cards(deck,boardstack[x])
+		switch_location(boardstack[x], 2)
 
 for x in range(7):
 	switch_postion(boardstack[x])
 
-print("Cards Left:", deck.top+1, "\n")
-print("",display_card(deck, deck.top),end = ' ')
+menu = {'D':deck, 'S':cardshownstack, 'A1':emptystack[0], 'A2':emptystack[1], 'A3':emptystack[2], 'A4':emptystack[3], '1':boardstack[0], '2':boardstack[1], '3':boardstack[2], '4':boardstack[3], '5':boardstack[4], '6':boardstack[5], '7':boardstack[6]}
 
+
+def show_rules(option):
+	print("Welcome to this solitaire game.")
+	print("how to play:")
+	print("1.Choose a stack to pick a card or chose \'D\' to reveal. ")
+	print("2.Choose a stack to place the card. ")
+	print("3.You cannot place cards in Deck and the other rules from regular solitaire game.")
+
+print("SOLITAIRE")
+#DISPLAY
+print("Cards Left:", deck.top + 1, "\n")
+print("    D                    S            A1     A2     A3     A4")
+print("",display_card(deck, deck.top), end = '   ')
 for x in reversed(range(3)):
-	print(display_card(cardshownstack, x), end = '')
-print("         ",end = ' ')
-
+	try:
+		print(display_card(cardshownstack, cardshownstack.top - x), end = '')
+	except IndexError :
+		print("[    ]", end = ' ')
+	
+print("        ",end = '')
 for x in range(len(emptystack)):
 	print(display_card(emptystack[x], emptystack[x].top), end = ' ')
 print("\n")
-
+print("                  1      2      3      4      5      6      7")
 for x in range(len(boardstack)):
 	print("              ", end = ' ')
-	for i in reversed((range(len(boardstack)))):
+	for i in range(len(boardstack)):
 		try:
-			print(display_card(boardstack[x], i), end = ' ')
+			print(display_card(boardstack[i], x), end = ' ')
 		except IndexError :
 			print("      ", end = ' ')
 	print()
-
 print()
+
+op = True
+while op: 
+	print("Choose a stack to pick a card or chose \'D\' to reveal: ")
+	f_input = input()#toupper	
+	print("Choose a stack to place the card : ")
+	s_input = input()#toupper
+
+	while (s_input == 'D'):
+		print("Cannot be placed in Deck! : ")
+		s_input = input()
+
+	if(check_location(menu[s_input]) ==  compare_color(menu[s_input], menu[f_input].S[menu[f_input].top])):
+		move_cards(menu[f_input], menu[s_input])
+		switch_location(menu[s_input], check_location(menu[s_input]))
+	else:
+		print("Unvalid Move!")
+
+	#DISPLAY
+	print("Cards Left:", deck.top + 1, "\n")
+	print("    D                    S            A1     A2     A3     A4")
+	print("",display_card(deck, deck.top), end = '   ')
+
+	for x in reversed(range(3)):
+		try:
+			print(display_card(cardshownstack, cardshownstack.top - x), end = '')
+		except IndexError :
+			print("[    ]", end = ' ')
+		
+	print("        ",end = '')
+
+	for x in range(len(emptystack)):
+		print(display_card(emptystack[x], emptystack[x].top), end = ' ')
+	print("\n")
+
+	print("                  1      2      3      4      5      6      7")
+	for x in range(len(boardstack)):
+		print("              ", end = ' ')
+		for i in range(len(boardstack)):
+			try:
+				print(display_card(boardstack[i], x), end = ' ')
+			except IndexError :
+				print("      ", end = ' ')
+		print()
+
+	print()	
+# if(f_input == 'D'):
+# 	for i in range(3):
+# 		move_cards(deck,cardshownstack)
